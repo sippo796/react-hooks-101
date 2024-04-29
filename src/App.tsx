2344d634f3356e1a6db5473b55d20755b32f0254
Satomi Watanabe <satomi.watanabe@japan-d2.com>
2024-04-29 12:40:02 +0900

    useState 101.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import React, {useState} from 'react'
 
 const App = () => {
 
+  const [count, setCount] = useState(0)
+  
   return (
-    <div>
-        This is a template for React app.
-    </div>
+    <>
+      <h1>Count: {count}</h1>
+      <div>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={() => setCount(prevCount => prevCount - prevCount)}>Reset</button>    
+      </div>
+      <div>
+        <button onClick={() => setCount(prevCount => prevCount * 2)}>x2</button>    
+      </div>
+      <div>
+        <button onClick={() => setCount(prevCount => prevCount % 3 === 0 ? prevCount / 3 : prevCount)}>3の倍数の時だけ3で割る</button>    
+      </div>
+    </>
   )
 }
 
 export default App
+
