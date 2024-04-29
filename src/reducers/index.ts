46b22851014ebb91883eaed527db8debb2a8537c
Satomi Watanabe <satomi.watanabe@japan-d2.com>
2024-04-29 17:09:15 +0900

    create reducer for events

---
+++ src/reducers/index.ts
@@ -0,0 +1,51 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: '2020年東京オリンピックのお知らせ',
+//   body: '2020年東京オリンピックを開催します！つきましては、、、',
+// }
+// # before
+// state = []
+// # after
+// state = [
+//   {id: 1, title: '2020年東京オリンピックのお知らせ', body: '2020年東京オリンピックを開催します！つきましては、、、'},
+// ]
+// # before
+// state = [
+//   {id: 1, title: 'タイトル1', body: 'ボディ1'},
+//   {id: 2, title: 'タイトル2', body: 'ボディ2'},
+//   {id: 3, title: 'タイトル3', body: 'ボディ3'},
+// ]
+// # after
+// state = [
+//   {id: 1, title: 'タイトル1', body: 'ボディ1'},
+//   {id: 2, title: 'タイトル2', body: 'ボディ2'},
+//   {id: 3, title: 'タイトル3', body: 'ボディ3'},
+//   {id: 4, title: '2020年東京オリンピックのお知らせ', body: '2020年東京オリンピックを開催します！つきましては、、、'},
+// ]
+
+type Event = {
+  id: number,
+  title: string,
+  body: string,
+  type: string
+}
+
+const events = (state:Event[] = [], action:Event) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      {
+        const event = { title: action.title, body: action.body }
+        const length = state.length
+        const id = length === 0 ? 1 : state[length - 1].id + 1
+        return [...state, {id, ...event}]
+      }
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENTS':
+      return []
+    default:
+      return state
+  }
+
+}
+
+export default events
