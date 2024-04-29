9fa36b4659f6360350b0d7b31fccc84cffa4f480
Satomi Watanabe <satomi.watanabe@japan-d2.com>
2024-04-29 16:01:32 +0900

    implement object state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React, {useState} from 'react'
+import { useState } from "react"
 
-const App = () => {
+type Props = {
+  name: string;
+  price: number;
+}
 
-  const [count, setCount] = useState(0)
-  
+const App = (defaultProps: Props) => {
+
+  const [state, setState] = useState(defaultProps)
+  const { name, price } = state
+
   return (
     <>
-      <h1>Count: {count}</h1>
-      <div>
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
-        <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
-      </div>
-      <div>
-        <button onClick={() => setCount(prevCount => prevCount - prevCount)}>Reset</button>    
-      </div>
-      <div>
-        <button onClick={() => setCount(prevCount => prevCount * 2)}>x2</button>    
-      </div>
-      <div>
-        <button onClick={() => setCount(prevCount => prevCount % 3 === 0 ? prevCount / 3 : prevCount)}>3の倍数の時だけ3で割る</button>    
-      </div>
+      <p>現在の[{name}]は{price}円です</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(defaultProps)}>Reset</button>
+      <input value={name} onChange={(e) => setState({...state, name:e.target.value})} />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App name='サンプル' price={1000}/>
   </React.StrictMode>,
 )
