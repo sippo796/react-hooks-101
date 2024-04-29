af7787c50f98029deee8ea0aa618c90307ab8d90
Satomi Watanabe <satomi.watanabe@japan-d2.com>
2024-04-29 16:46:49 +0900

    create event from and event table with bootstrap

---
+++ src/components/App.tsx
@@ -0,0 +1,50 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+
+  return (
+    <div className="container-fluid">
+        <h4>イベント作成フォーム</h4>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formEventTitle">タイトル</label>
+            <input className="form-control" id="formEventTitle" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="formEventBody">ボディ</label>
+            <textarea className="form-control" id="formEventBody" />
+          </div>
+
+          <button className='btn btn-primary'>イベントを作成する</button>
+          <button className='btn btn-danger'>全てのイベントを削除する</button>
+        </form>
+
+        <h4>イベント一覧</h4>
+        <table className='table table-hover'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>ボディ</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>タイトル1</td>
+              <td>ボディ1</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>タイトル2</td>
+              <td>ボディ2</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default App
