6674a94038671829cf2276e56979c730be7bb147
Satomi Watanabe <satomi.watanabe@japan-d2.com>
2024-05-04 12:38:16 +0900

    install redux

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -23,6 +23,8 @@
 //   {id: 4, title: '2020年東京オリンピックのお知らせ', body: '2020年東京オリンピックを開催します！つきましては、、、'},
 // ]
 
+import { CREATE_EVENT, DELETE_ALL_EVENTS, DELETE_EVENT } from "../actions"
+
 export type State = {
   id: number,
   title?: string,
@@ -36,18 +38,18 @@ export type Action = {
 
 const events = (state:State[], action:Action) => {
   switch(action.type) {
-    case 'CREATE_EVENT':
+    case CREATE_EVENT:
       {
         const event = { title: action.payload.title, body: action.payload.body }
         const length = state.length
         const id = length === 0 ? 1 : state[length - 1].id + 1
         return [...state, {id, ...event}]
       }
-    case 'DELETE_EVENT':
+    case DELETE_EVENT:
       {
         return state.filter(event => event.id !== action.payload.id)
       }
-    case 'DELETE_ALL_EVENTS':
+    case DELETE_ALL_EVENTS:
       return []
     default:
       return state
